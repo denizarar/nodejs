0633b219dbb85e5fb23cd1d1aeda3a1570a40bc6
Deniz Arar <39047354+denizarar@users.noreply.github.com>
2021-09-19 14:28:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express"); // Acquire the express package and assign it
 const app = express(); // Calls the method "express()" and assigns it's output to "app". "express()" will create an express app for you.
 
 app.get("/", (req, res) => { // Creates sort of a listener for when there are "GET" requests to the "/" (root) path. Takes in req (request) and res (response)
-    res.send("Hello world-Test2!"); // For the response, send a string "Hello World!"
+    res.send("Hello world-Test3!"); // For the response, send a string "Hello World!"
 });
 
 app.listen(3000, () => { // Tells the app to start on port 3000. This function below is run when
